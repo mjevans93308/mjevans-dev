e548cea2011b3e9a3a06a4e48f75c5b69d824957
Matthew Evans <me@mjevans.dev>
2021-07-15 10:16:14 -0700

    main: added next.config.js with support for plugins and gh-pages asset fix

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+module.exports = withPlugins([
+    [optimizedImages, {
+        mozjpeg: {
+            quality: 80,
+        },
+        pngquant: {
+            speed: 3,
+            strip: true,
+            verbose: true,
+        },
+        imagesPublicPath: '/mjevans-dev/_next/static/images/',
+    }],
+    {
+        assetPrefix: '/mjevans-dev/',
+        basePath: '/mjevans-dev',
+        env,
+    },
+]);
