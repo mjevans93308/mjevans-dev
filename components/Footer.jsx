42203abb139c67630ae99873117916b359111172
Matthew Evans <me@mjevans.dev>
2021-12-05 04:05:18 -0700

    updated footer and license to year 2022

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export const Footer = () => {
     return (
         <footer className="flex items-center justify-around w-full p-6 h-24 border-t text-gray-500 font-normal text-md bg-gray-100">
-            <span>© Matthew Evans 2021</span>
+            <span>© Matthew Evans 2022</span>
             <div className="flex justify-between">
                 <a href="https://twitter.com/mevans93308" target="_blank" rel="noopener" className="px-2"><FontAwesomeIcon icon={faTwitter} /></a>
                 <a href="https://linkedin.com/in/matthew-evans-a177838a" target="_blank" rel="noopener" className="px-2"><FontAwesomeIcon icon={faLinkedin} /></a>
